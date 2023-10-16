4ff38c1233a3127668b2ad276f7925176b8b329d
michaelma7 <michaelma16688@gmail.com>
2023-10-16 12:42:41 -0400

    form control for js and html added form to html added eventlisteners and function to handle form inputs added way to reset form

--- library.js
+++ library.js
@@ -1,6 +1,19 @@
 const myLibrary = [];
 
-const addNew = querySelector(".addBook").addEventListener("click", (e) => modalPopup());
+//form inputs
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookLength = document.getElementById('pages');
+const bookRead = document.getElementById('read');
+const newBookForm = document.getElementById('newBook');
+
+const modal = document.querySelector("dialog");
+const addNew = document.querySelector(".addBook").addEventListener("click", (e) => modal.showModal());
+const create = document.querySelector(".create").addEventListener("click", (e) => { 
+                                                                                    e.preventDefault(); 
+                                                                                    addBookToLibrary(bookTitle.value, bookAuthor.value, bookLength.value, bookRead.value);
+                                                                                    modal.close();
+                                                                                });
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -9,10 +22,13 @@ function Book (title, author, pages, read) {
     this.read = read;
 }
 
-function addBooktoLibrary() {
+function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     // refresh table with new value
+    showLibrary();
+    newBookForm.reset();
+    return
 }
 
 function showLibrary() {
