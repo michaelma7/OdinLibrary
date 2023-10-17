713d0c6b83f838e50cdfc48eeda6c3915da19249
michaelma7 <michaelma16688@gmail.com>
2023-10-17 12:42:43 -0400

    1.0 html js fixed table setup: thead properly defined so rows add in tbody
    made new functions to display table everytime a book is added/removed
    added test books for css styling
    added function to handle read status
    added associated selectors for above

--- library.js
+++ library.js
@@ -5,8 +5,11 @@ const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookLength = document.getElementById('pages');
 const bookRead = document.getElementById('read');
-const newBookForm = document.getElementById('newBook');
 
+
+const newBookForm = document.getElementById('newBook');
+const table = document.querySelector('.library');
+const rows = document.querySelector('.rows');
 const modal = document.querySelector("dialog");
 const addNew = document.querySelector(".addBook").addEventListener("click", (e) => modal.showModal());
 const create = document.querySelector(".create").addEventListener("click", (e) => { 
@@ -22,15 +25,49 @@ function Book (title, author, pages, read) {
     this.read = read;
 }
 
+function displayLibrary() {
+    rows.replaceChildren();
+    myLibrary.forEach(book=> {
+        let row = rows.insertRow();
+        let title = row.insertCell();
+        let author = row.insertCell();
+        let page = row.insertCell(); 
+        let read = row.insertCell();
+        let action = row.insertCell();
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        page.innerHTML = book.pages;
+        read.innerHTML = book.read;
+        action.innerHTML = "<button class='status' name='" + (myLibrary.length-1) + "'>Set Read</button> <button class='delete' id='" 
+                            + (myLibrary.length-1) + "'>Remove</button>";
+        let button = document.querySelector('.status').addEventListener('click', (e) => {changeReadStatus(e.target.name);});
+        let remove = document.getElementById(`${myLibrary.length-1}`).addEventListener('click', (e) => {removeBook(e.target);});  
+    });
+}
+
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    // refresh table with new value
-    showLibrary();
     newBookForm.reset();
-    return
+    displayLibrary();
 }
 
-function showLibrary() {
-    
+function changeReadStatus(index) {
+    let current = myLibrary[index].read;
+    if (current === 'Yes') {
+        myLibrary[index].read = 'No';
+    } else {
+        myLibrary[index].read = 'Yes';
+    };
+    //refresh display. can i do it without deleting everything?
+    displayLibrary(); 
 }
+
+function removeBook(button) {
+    let row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+addBookToLibrary('romanov', 'simon', 1000, 'No');
+addBookToLibrary('Sowrd of Islam', 'Tom Holland', 1000, 'No');
+addBookToLibrary('The Sunlit Man', 'Brandon Sanderson', 800, 'Yes');
+addBookToLibrary('pain', 'itch', 500, 'Yes');
