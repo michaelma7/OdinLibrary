cc45a81d13cc2d37bbaf6ea9120c3c715d0663db
michaelma7 <michaelma16688@gmail.com>
2023-12-26 20:17:10 -0500

    class roll out v1.1
    JS:
    refactored book and library into class
    
    css:
    changed color of button text

--- library.js
+++ library.js
@@ -1,33 +1,63 @@
-const myLibrary = [];
-
 //form inputs
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookLength = document.getElementById('pages');
 const bookRead = document.getElementById('read');
 
-
+//other DOM cache
 const newBookForm = document.getElementById('newBook');
 const table = document.querySelector('.library');
 const rows = document.querySelector('.rows');
 const modal = document.querySelector("dialog");
+
+//event listeners
 const addNew = document.querySelector(".addBook").addEventListener("click", (e) => modal.showModal());
 const create = document.querySelector(".create").addEventListener("click", (e) => { 
                                                                                     e.preventDefault(); 
-                                                                                    addBookToLibrary(bookTitle.value, bookAuthor.value, bookLength.value, bookRead.value);
+                                                                                    myLibrary.addBookToLibrary(bookTitle.value, bookAuthor.value, bookLength.value, bookRead.value);
                                                                                     modal.close();
                                                                                 });
 
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+class Library {
+    constructor() {
+        this.shelf = [];
+    }
+
+    addBookToLibrary() {
+        let newBook = new Book(title, author, pages, read);
+        this.shelf.push(newBook);
+        newBookForm.reset();
+        displayLibrary();
+    }
+
+    changeReadStatus(index) {
+        let current = this.shelf[index].read
+        if(current === 'Yes') {
+            this.shelf[index].read = 'No';
+        } else {
+            this.shelf[index].read = 'Yes';
+        };
+        displayLibrary();
+    }
+
+    removeBook(index) {
+        this.shelf.splice(index, 1);
+        displayLibrary();
+    }
 }
 
 function displayLibrary() {
     rows.replaceChildren();
-    myLibrary.forEach(book=> {
+    myLibrary.shelf.forEach(book=> {
         let row = rows.insertRow();
         let title = row.insertCell();
         let author = row.insertCell();
@@ -38,32 +68,11 @@ function displayLibrary() {
         author.innerHTML = book.author;
         page.innerHTML = book.pages;
         read.innerHTML = book.read;
-        action.innerHTML = "<button class='status' name='" + (myLibrary.length-1) + "'>Set Read</button> <button class='delete' id='" 
-                            + (myLibrary.length-1) + "'>Remove</button>";
-        let button = document.querySelector('.status').addEventListener('click', (e) => {changeReadStatus(e.target.name);});
-        let remove = document.getElementById(`${myLibrary.length-1}`).addEventListener('click', (e) => {removeBook(e.target);});  
+        action.innerHTML = "<button class='status' name='" + (myLibrary.shelf.length-1) + "'>Set Read</button> <button class='delete' id='" 
+                            + (myLibrary.shelf.length-1) + "'>Remove</button>";
+        let button = document.querySelector('.status').addEventListener('click', (e) => {myLibrary.changeReadStatus(e.target.name);});
+        let remove = document.getElementById(`${myLibrary.shelf.length-1}`).addEventListener('click', (e) => {myLibrary.removeBook(e.target.id);});  
     });
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    newBookForm.reset();
-    displayLibrary();
-}
-
-function changeReadStatus(index) {
-    let current = myLibrary[index].read;
-    if (current === 'Yes') {
-        myLibrary[index].read = 'No';
-    } else {
-        myLibrary[index].read = 'Yes';
-    };
-    //refresh display. can i do it without deleting everything?
-    displayLibrary(); 
-}
-
-function removeBook(button) {
-    let row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-}
+const myLibrary = new Library();
